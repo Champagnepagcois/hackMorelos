f9758c990733b86ea5b246ca0f539a26cbe02ba5
marlon <marlonrodriguez2b@gmail.com>
2024-05-17 10:40:55 -0600

    ci: vista chat

--- app/michat/page.js
+++ app/michat/page.js
@@ -1,32 +1,40 @@
-'use client'
-import React, { useState } from 'react';
-import { makeStyles } from '@mui/styles';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
+"use client";
+import React, { useState } from "react";
+import { makeStyles } from "@mui/styles";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexDirection: 'column',
-    height: '100%',
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
     padding: 16, // Aquí se utilizan valores numéricos en lugar de theme.spacing
   },
   messagesContainer: {
     flexGrow: 1,
-    overflowY: 'auto',
+    overflowY: "auto",
     marginBottom: 16, // También aquí
   },
   message: {
     padding: 8, // Y aquí
     margin: 8, // Y aquí
     borderRadius: 4, // Y aquí
-    backgroundColor: '#F2F2F2',
+    backgroundColor: "#F2F2F2",
+  },
+  messageBot: {
+    padding: 8, // Y aquí
+    margin: 8, // Y aquí
+    borderRadius: 4, // Y aquí
+    backgroundColor: "#5aa9e6",
+    color:"#FFF",
   },
   inputContainer: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
   },
   textField: {
     flexGrow: 1,
@@ -36,42 +44,49 @@ const useStyles = makeStyles((theme) => ({
 
 const ChatInterface = () => {
   const classes = useStyles();
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
+  const [prompt, setPrompt] = useState("");
+  const [messages, setMessages] = useState([{message:"hola, me podrias ayudar a encontrar un médico?", user:1}]);
 
   const handleMessageChange = (event) => {
-    setMessage(event.target.value);
+    setPrompt(event.target.value);
   };
 
   const handleMessageSend = () => {
-    if (message.trim() !== '') {
-      setMessages([...messages, message]);
-      setMessage('');
+    if (prompt.trim() !== "") { // Cambio aquí, usando message en lugar de prompt
+      const newMessage = { user: 1, message: prompt }; // Crear un nuevo mensaje con el texto
+      setMessages([...messages, newMessage]); // Agregar el nuevo mensaje al array de mensajes
+      setPrompt(""); // Limpiar el campo de entrada después de enviar el mensaje
     }
   };
 
   return (
-    <Paper className={classes.root}>
-      <div className={classes.messagesContainer}>
-        {messages.map((msg, index) => (
-          <Paper key={index} className={classes.message}>
-            <Typography variant="body1">{msg}</Typography>
-          </Paper>
-        ))}
-      </div>
-      <div className={classes.inputContainer}>
-        <TextField
-          className={classes.textField}
-          label="Type your message"
-          variant="outlined"
-          value={message}
-          onChange={handleMessageChange}
-        />
-        <Button variant="contained" color="primary" onClick={handleMessageSend}>
-          Send
-        </Button>
-      </div>
-    </Paper>
+    <Box sx={{ height: "100vh", width: "100vw" }}>
+      <Paper className={classes.root}>
+        <div className={classes.messagesContainer}>
+          {messages.map((msg, index) => (
+            <Paper key={index} className={msg.user%2==0?classes.messageBot:classes.message}>
+              <Typography variant="body1">{msg.message}</Typography>
+            </Paper>
+          ))}
+        </div>
+        <div className={classes.inputContainer}>
+          <TextField
+            className={classes.textField}
+            label="Type your message"
+            variant="outlined"
+            value={prompt}
+            onChange={handleMessageChange}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleMessageSend}
+          >
+            Send
+          </Button>
+        </div>
+      </Paper>
+    </Box>
   );
 };
 
