6cc3a9123403ffc723ecd5c342180f563d9d702b
marlon <marlonrodriguez2b@gmail.com>
2024-05-18 05:13:59 -0600

    fix:web view problem

--- app/helpers/gpt/ApiGpt.js
+++ app/helpers/gpt/ApiGpt.js
@@ -1,7 +1,7 @@
 // api/chatApi.js
 import OpenAI from "openai";
 import Prompt from "../gpt/promps";
-const openai = new OpenAI({apiKey:process.env.GPT_KEY,dangerouslyAllowBrowser:true});
+const openai = new OpenAI({apiKey:'sk-proj-UvTFwOq5hJzRHtm0WFrJT3BlbkFJ87Q3YFh9DmtbLvICisZE',dangerouslyAllowBrowser:true});
 
 const GPT = {};
 
